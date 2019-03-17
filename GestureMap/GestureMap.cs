0d55242c66d9812667b1e4dabc4412c6b9740196
Ben-Floyd <bjfloyd@LAPTOP-AKINLJMT.localdomain>
2019-03-17 00:15:31 -0500

    Proto GestureMap Framework

---
+++ GestureMap/GestureMap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace LeapMotionGestureMap
+{
+
+    public class GestureMap
+    {
+        private static Leap.Controller controller;
+        public static Leap.GestureList standardGestures;
+
+        private LeapListener listener;
+
+        public GestureMap()
+        {
+            controller = new Leap.Controller();
+            listener = new LeapListener();
+
+            controller.EnableGesture(Leap.Gesture.GestureType.TYPE_SWIPE);
+            controller.AddListener(listener);
+
+            standardGestures = null;
+        }
+
+        public static void processFrame()
+        {
+            Leap.Frame frame = controller.Frame();
+            standardGestures = frame.Gestures();
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
--- PowerPointAdd-in/ThisAddIn.cs
+++ PowerPointAdd-in/ThisAddIn.cs
@@ -5,29 +5,87 @@ using System.Text;
 using System.Xml.Linq;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 using Office = Microsoft.Office.Core;
-using Leap;
 
 namespace LeapMotionPowerPointAdd_in
 {
-    
+
     public partial class ThisAddIn
     {
-        Controller controller;
-        LeapEventListener listener;
+        private LeapMotionGestureMap.GestureMap gestureMap;
+
+        private bool slideShowRunnning;
+
+        private Object thisLock = new Object();
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            controller = new Controller();
-            listener = new LeapEventListener(this.Application);
-            controller.AddListener(listener);
+            slideShowRunnning = false;
+            gestureMap = new LeapMotionGestureMap.GestureMap();
 
-            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            Application.SlideShowBegin +=
+                new PowerPoint.EApplication_SlideShowBeginEventHandler(
+                    SlideShowStart);
+
+            Application.SlideShowEnd +=
+                new PowerPoint.EApplication_SlideShowEndEventHandler(
+                    SlideShowEnd);
+
+            Application.PresentationNewSlide +=
+                new PowerPoint.EApplication_PresentationNewSlideEventHandler(
+                NewSlide);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
-            controller.RemoveListener(listener);
-            controller.Dispose();
+        }
+
+        void NewSlide(PowerPoint.Slide slide)
+        {
+            PowerPoint.Shape textBox = slide.Shapes.AddTextbox(
+                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
+            textBox.TextFrame.TextRange.InsertAfter("Hello");
+        }
+
+        void SlideShowStart(PowerPoint.SlideShowWindow window)
+        {
+            slideShowRunnning = true;
+
+            while (slideShowRunnning)
+            {
+                if ((LeapMotionGestureMap.GestureMap.standardGestures != null) 
+                    && !(LeapMotionGestureMap.GestureMap.standardGestures.IsEmpty))
+                {
+                    foreach(Leap.Gesture gesture in LeapMotionGestureMap.GestureMap.standardGestures)
+                    {
+                        HandleGesture(gesture);
+                    }
+                }
+            }
+        }
+
+        void SlideShowEnd(PowerPoint.Presentation presentation)
+        {
+            slideShowRunnning = false;
+        }
+
+        void HandleGesture(Leap.Gesture gesture)
+        {
+            if (gesture.Type.Equals(Leap.Gesture.GestureType.TYPE_SWIPE))
+            {
+                Leap.SwipeGesture swipe = new Leap.SwipeGesture(gesture);
+
+                if (gesture.State.Equals(Leap.Gesture.GestureState.STATESTOP))
+                {
+                    if (swipe.Direction.x > 0)
+                    {
+                        Application.ActivePresentation.SlideShowWindow.View.Next();
+                    }
+                    else if (!Application.ActivePresentation.SlideShowWindow.View.CurrentShowPosition.Equals(0))
+                    {
+                        Application.ActivePresentation.SlideShowWindow.View.Previous();
+                    }
+                }
+            }
         }
 
         #region VSTO generated code
