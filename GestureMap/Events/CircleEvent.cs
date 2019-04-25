ddb71e5f81babbc03222b5b39b5c8eddd7879a26
Ben-Floyd <bjfloyd@LAPTOP-AKINLJMT.localdomain>
2019-04-25 14:00:48 -0500

    Lense Launcher

---
+++ GestureMap/Events/CircleEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapMotionGestureMap.Events
+{
+    public class CircleEvent : EventArgs
+    {
+        private Leap.CircleGesture _circle;
+
+        public CircleEvent(Leap.CircleGesture circle)
+        {
+            _circle = circle;
+        }
+
+        public Leap.CircleGesture Swipe
+        {
+            get { return _circle; }
+            set { _circle = value; }
+        }
+    }
+}
